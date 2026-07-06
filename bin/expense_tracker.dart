ba11526545464d565ba328dbfdea992dfeb2bebd
Rohan2002-33 <mahirrohan08@gmail.com>
2026-07-06 13:47:28 +0600

    Add CLI main menu loop

--- bin/expense_tracker.dart
+++ bin/expense_tracker.dart
@@ -1,5 +1,35 @@
-import 'package:expense_tracker/expense_tracker.dart' as expense_tracker;
+import 'dart:io';
+import 'package:expense_tracker/expense_tracker.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${expense_tracker.calculate()}!');
+void main() {
+  final tracker = ExpenseTracker();
+  bool running = true;
+
+  while (running) {
+    print('===== Expense Tracker =====');
+    print('1. Add Expense');
+    print('2. View All Expenses');
+    print('3. Show Total Expenses');
+    print('4. Exit');
+    stdout.write('Choose Option: ');
+    String? choice = stdin.readLineSync();
+
+    switch (choice) {
+      case '1':
+        tracker.addExpense();
+        break;
+      case '2':
+        tracker.viewExpenses();
+        break;
+      case '3':
+        tracker.showTotal();
+        break;
+      case '4':
+        running = false;
+        print('Thank you for using Expense Tracker!');
+        break;
+      default:
+        print('Invalid option, try again.\n');
+    }
+  }
 }
